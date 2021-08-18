f58aa8539f286c217092132b2bd17e9e11c07a70
Ityna Ivanytska <ira.pechenitsya@gmal.com>
2021-08-18 08:54:44 +0300

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && (array.length) > 0)  {
+    let minVal = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (minVal > array[i]) {
+        minVal = array[i]
+      }
+    }
+    return minVal;
+  } else return 0;
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && (array.length) > 0)  {
+    let maxVal = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (maxVal < array[i]) {
+        maxVal = array[i]
+      }
+    }
+    return maxVal;
+  } else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) && (array.length) > 0)  {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum/array.length;
+  } else return 0;
+
 }
